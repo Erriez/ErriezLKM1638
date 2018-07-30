f69648506e2a2bfd22702ce65f7c9df00db0a151
Erriez <Erriez@users.noreply.github.com>
2018-07-30 23:09:21 +0200

    Add .nojekyll file to allow Doxygen filenames starting with a _.

---
+++ docs/search/variables_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['_5fdots',['_dots',['../class_l_k_m1638_board.html#a2de77cd6c33672edb5a3288b09cdcbfc',1,'LKM1638Board']]],
+  ['_5fleds',['_leds',['../class_l_k_m1638_board.html#ad0ecb25b0069693153b4c99b1fd0c0c6',1,'LKM1638Board']]],
+  ['_5fpos',['_pos',['../class_l_k_m1638_board.html#a1dc0720aa961510a147cb7e3db7d7873',1,'LKM1638Board']]]
+];
